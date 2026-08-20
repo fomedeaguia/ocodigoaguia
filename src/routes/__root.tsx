cbc697d419a3d96bcde385b875d39a5451563b76
leoalvarengam <leonardoalvarengam@gmail.com>
2026-08-20 14:54:46 -0300

    fix(css): load global styles in root document

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,35 @@
-import { createRootRoute, Outlet, ScrollRestoration } from "@tanstack/react-router";
-import "../styles.css";
+import {
+  createRootRoute,
+  HeadContent,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@tanstack/react-router";
+import appStylesHref from "../styles.css?url";
 
 export const Route = createRootRoute({
   component: Root,
 });
 
+export const links = () => [
+  {
+    rel: "stylesheet",
+    href: appStylesHref,
+  },
+];
+
 function Root() {
   return (
-    <>
-      <ScrollRestoration />
-      <Outlet />
-    </>
+    <html lang="pt-BR">
+      <head>
+        <HeadContent />
+        <link rel="stylesheet" href={appStylesHref} />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
   );
 }
